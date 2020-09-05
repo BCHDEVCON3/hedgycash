0d07ff086d026267531801ead10c54ad8313f63f
Andre Cabrera <andrecabrera@protonmail.ch>
2020-09-05 15:00:32 -0300

    wip: handler orderbook

--- orderbook/src/index.handler.js
+++ orderbook/src/index.handler.js
@@ -4,6 +4,10 @@ const httpErrorHandler = require("@middy/http-error-handler");
 
 const createHandler = (func) => middy(func).use(httpErrorHandler()).use(cors());
 
-module.exports.hello = createHandler((event, context, callback) => {
+module.exports.createContract = createHandler((event, context, callback) => {
+  callback(null, { statusCode: 200, body: "Hello!" });
+});
+
+module.exports.fundContract = createHandler((event, context, callback) => {
   callback(null, { statusCode: 200, body: "Hello!" });
 });
