47fb2ea4b6f3dde04375b7e4254d1e39a28aea17
Natan <natanscamargos@gmail.com>
2020-09-05 19:56:15 -0300

    orderbook setup

--- orderbook/src/orders/orders.handler.js
+++ orderbook/src/orders/orders.handler.js
@@ -1,18 +1,31 @@
-const middy = require("@middy/core");
-const cors = require("@middy/http-cors");
-const httpErrorHandler = require("@middy/http-error-handler");
+const middy = require('@middy/core');
+const cors = require('@middy/http-cors');
+const httpErrorHandler = require('@middy/http-error-handler');
+const ordersService = require('./orders.service');
 
 const createHandler = (func) => middy(func).use(httpErrorHandler()).use(cors());
 
-module.exports.createContract = createHandler((event, context, callback) => {
-  callback(null, { statusCode: 200, body: "Hello!" });
+module.exports.create = createHandler((event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    ordersService
+        .create(event.body)
+        .then((result) => {
+            callback(null, { statusCode: 200, body: JSON.stringify(result) });
+        })
+        .catch((err) =>
+            callback(null, {
+                statusCode: err.statusCode || 500,
+                body: JSON.stringify(err),
+            }),
+        );
 });
 
-module.exports.fundContract = createHandler((event, context, callback) => {
-  callback(null, { statusCode: 200, body: "Hello!" });
+module.exports.fund = createHandler((event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    callback(null, { statusCode: 200, body: 'Hello!' });
 });
 
-module.exports.listOrders = createHandler((event, context, callback) => {
-  callback(null, { statusCode: 200, body: "Hello!" });
+module.exports.list = createHandler((event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false;
+    callback(null, { statusCode: 200, body: 'Hello!' });
 });
-
--- orderbook/src/orders/orders.service.js
+++ orderbook/src/orders/orders.service.js
@@ -1,22 +1,30 @@
-const validator = require("validator");
-const axios = require("axios");
-const vm = require("vm");
-const BITBOX = require("bitbox-sdk").BITBOX;
-const bitcore = require("bitcore-lib-cash");
-const Order = require("./order.repository");
+const validator = require('validator');
+const axios = require('axios');
+const vm = require('vm');
+const BITBOX = require('bitbox-sdk').BITBOX;
+const bitcore = require('bitcore-lib-cash');
+const Order = require('./order.repository');
 
 const bitbox = new BITBOX();
 
 const MIN_ORACLE_BALANCE = 0.001;
 
 class orderService {
-  create = async () => {
-   
-  };
+    create = async (rawPayment) => {
+        try {
+            const payment = PaymentProtocol.Payment.decode(rawPayment);
+            return payment;
+        } catch (err) {
+            return Promise.reject({
+                statusCode: 400,
+                message: 'Invalid payment protocol object. Cause: ' + err.message,
+            });
+        }
+    };
 
-  list = () => {
-    return Order.find();
-  };
+    list = () => {
+        return Order.find();
+    };
 }
 
 module.exports = new orderService();
