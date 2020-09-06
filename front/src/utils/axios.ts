bc89ba8e4642c0cee1bd07a939186ceb5e112b7b
Sillas Camara <sillasmc@gmail.com>
2020-09-06 11:34:36 -0300

    add https

--- front/src/utils/axios.ts
+++ front/src/utils/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const ORDERS_API = 'http://uyfx577r6j.execute-api.us-east-1.amazonaws.com/dev/api/rest';
+export const ORDERS_API = 'https://uyfx577r6j.execute-api.us-east-1.amazonaws.com/dev/api/rest';
 export const ORACLES_API = 'https://uzs2lnqnqe.execute-api.us-east-1.amazonaws.com/dev/api/rest';
 
 export const oraclesApi = axios.create({
