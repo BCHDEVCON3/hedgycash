f97eed59104e8855216cf62ebed68d8bd05de30f
Sillas Camara <sillasmc@gmail.com>
2020-09-06 10:59:03 -0300

    Change new contract fields

--- front/src/pages/NewContract/NewContract.tsx
+++ front/src/pages/NewContract/NewContract.tsx
@@ -29,55 +29,47 @@ import bitcoincomLink from 'bitcoincom-link';
 import './NewContract.css';
 
 const NewContract: React.FC = () => {
-    const [orderType, setOrderType] = useState<string>('Hedge');
+    const initialOrderState = { maturity: '', highMultiplier: '', lowMultiplier: '' };
+
     const [asset, setAsset] = useState<string>();
-    const [bchValue, setBCHValue] = useState<number>();
-    const [assetValue, setAssetValue] = useState<number>();
     const [selectedOracle, setSelectedOracle] = useState(null);
-    const [debug, setDebug] = useState(false)
-    const [debugMessage, setDebugMessage] = useState('')
+    const [debug, setDebug] = useState(false);
+    const [debugMessage, setDebugMessage] = useState('');
+    const [orderState, setOrderState] = React.useState(initialOrderState);
+
     const { oracles } = useSelector((state: RootState) => state.oraclesState);
 
     const dispatch = useDispatch();
 
-    const onBCHInputChanged = (e: CustomEvent) => {
-        setBCHValue(e.detail.value);
-    };
-
-    const onAssetInputChanged = (e: CustomEvent) => {
-        setAssetValue(e.detail.value);
-    };
-
     const onSubmitOrderButtonClicked = () => {
-        console.log(orderType);
-        console.log(bchValue);
-        console.log(assetValue);
-        console.log(asset);
-  
-        (bitcoincomLink as any).getAddress({ protocol: 'BCH' }).then(() => (bitcoincomLink as any).sendAssets({
-            to: 'bitcoincash:qrd9khmeg4nqag3h5gzu8vjt537pm7le85lcauzezc',
-            protocol: 'BCH',
-            value: '0.000123', })
-        //   })payInvoice({
-        //   //  url: 'http://1a4adcc7a8ec.ngrok.io/dev/api/rest/createPaymentRequest?amount=546',
-        //   url: 'bitcoincash:?r=http://1a4adcc7a8ec.ngrok.io/dev/api/rest/createPaymentRequest?amount=546'
-        //     })
-            .then((data: any) => {
-            const {
-                memo,
-            } = data;
-            setDebugMessage('foi')
-            setDebug(true)
-            console.log('Payment processed memo from merchant server: ' + memo);
-            })
-            .catch(( e ) => {
-                console.log('ERRORRR')
-                setDebugMessage(JSON.stringify(e))
-                setDebug(true)
-               console.log(e)
-            }))
-
+        (bitcoincomLink as any).getAddress({ protocol: 'BCH' }).then(() =>
+            (bitcoincomLink as any)
+                .sendAssets({
+                    to: 'bitcoincash:qrd9khmeg4nqag3h5gzu8vjt537pm7le85lcauzezc',
+                    protocol: 'BCH',
+                    value: '0.000123',
+                })
+                //   })payInvoice({
+                //   //  url: 'http://1a4adcc7a8ec.ngrok.io/dev/api/rest/createPaymentRequest?amount=546',
+                //   url: 'bitcoincash:?r=http://1a4adcc7a8ec.ngrok.io/dev/api/rest/createPaymentRequest?amount=546'
+                //     })
+                .then((data: any) => {
+                    const { memo } = data;
+                    setDebugMessage('foi');
+                    setDebug(true);
+                    console.log('Payment processed memo from merchant server: ' + memo);
+                })
+                .catch((e) => {
+                    console.log('ERRORRR');
+                    setDebugMessage(JSON.stringify(e));
+                    setDebug(true);
+                    console.log(e);
+                }),
+        );
     };
+
+    const isDisabled = () =>
+        !asset || !orderState.maturity || !orderState.highMultiplier || !orderState.lowMultiplier;
 
     useEffect(() => {
         dispatch(fetchOraclesInit());
@@ -99,61 +91,16 @@ const NewContract: React.FC = () => {
             </IonHeader>
             <IonContent fullscreen>
                 <IonRow>
-                    <IonCol
-                        size-sm="12"
-                        offsetLg="3"
-                        sizeLg="6"
-                    >
+                    <IonCol size-sm="12" offsetLg="3" sizeLg="6">
                         <AssetChart oracle={selectedOracle} />
                     </IonCol>
-                    <IonCol
-                         size="12"
-                         offsetLg="3"
-                         sizeLg="6"
-                    >
+                    <IonCol size="12" offsetLg="3" sizeLg="6">
                         <IonCard id="orderCard">
                             <IonCardHeader>
                                 <IonCardTitle id="orderCardTitle">New Contract</IonCardTitle>
                             </IonCardHeader>
                             <IonCardContent id="orderCardContent">
                                 <IonRow>
-                                    <IonCol size="6">
-                                        <IonText>
-                                            <h2>{orderType} in BCH:</h2>
-                                        </IonText>
-                                        <IonInput
-                                            onIonChange={onBCHInputChanged}
-                                            type="number"
-                                            inputMode="decimal"
-                                            placeholder="0.0"
-                                        />
-                                    </IonCol>
-                                    <IonCol size="6">
-                                        <IonText>
-                                            <h2>Select Order Type:</h2>
-                                        </IonText>
-                                        <IonSelect
-                                            interface="popover"
-                                            value={orderType}
-                                            onIonChange={(e) => setOrderType(e.detail.value)}
-                                        >
-                                            <IonSelectOption value="Hedge">Hedge</IonSelectOption>
-                                            <IonSelectOption value="Short">Short</IonSelectOption>
-                                        </IonSelect>
-                                    </IonCol>
-                                </IonRow>
-                                <IonRow>
-                                    <IonCol size="6">
-                                        <IonText>
-                                            <h2>Value in {asset}:</h2>
-                                        </IonText>
-                                        <IonInput
-                                            onIonChange={onAssetInputChanged}
-                                            type="number"
-                                            inputMode="decimal"
-                                            placeholder="0.0"
-                                        />
-                                    </IonCol>
                                     <IonCol size="6">
                                         <IonText>
                                             <h2>Select Asset:</h2>
@@ -180,11 +127,72 @@ const NewContract: React.FC = () => {
                                             ))}
                                         </IonSelect>
                                     </IonCol>
+                                    <IonCol size="6">
+                                        <IonText>
+                                            <h2>Number of Blocks for Maturity</h2>
+                                        </IonText>
+                                        <IonInput
+                                            onIonChange={(e) =>
+                                                setOrderState((prev) => ({
+                                                    ...prev,
+                                                    maturity: e.detail.value!,
+                                                }))
+                                            }
+                                            type="number"
+                                            inputMode="numeric"
+                                            value={orderState.maturity}
+                                            min="0"
+                                            placeholder="0"
+                                            required
+                                        />
+                                    </IonCol>
+                                </IonRow>
+                                <IonRow>
+                                    <IonCol size="6">
+                                        <IonText>
+                                            <h2>High Liquidation Price Multiplier (%)</h2>
+                                        </IonText>
+                                        <IonInput
+                                            onIonChange={(e) =>
+                                                setOrderState((prev) => ({
+                                                    ...prev,
+                                                    highMultiplier: e.detail.value!,
+                                                }))
+                                            }
+                                            type="number"
+                                            inputMode="numeric"
+                                            value={orderState.highMultiplier}
+                                            min="1"
+                                            placeholder="1"
+                                            required
+                                        />
+                                    </IonCol>
+                                    <IonCol size="6">
+                                        <IonText>
+                                            <h2>Low Liquidation Price Multiplier (%)</h2>
+                                        </IonText>
+                                        <IonInput
+                                            onIonChange={(e) =>
+                                                setOrderState((prev) => ({
+                                                    ...prev,
+                                                    lowMultiplier: e.detail.value!,
+                                                }))
+                                            }
+                                            type="number"
+                                            inputMode="numeric"
+                                            value={orderState.lowMultiplier}
+                                            min="1"
+                                            max="99"
+                                            placeholder="1"
+                                            required
+                                        />
+                                    </IonCol>
                                 </IonRow>
                                 <IonRow>
                                     <IonCol>
                                         <IonButton
                                             id="submitOrder"
+                                            disabled={isDisabled()}
                                             color="success"
                                             expand="full"
                                             onClick={onSubmitOrderButtonClicked}
@@ -197,7 +205,6 @@ const NewContract: React.FC = () => {
                         </IonCard>
                     </IonCol>
                 </IonRow>
-               
             </IonContent>
             <IonToast
                 id=""
